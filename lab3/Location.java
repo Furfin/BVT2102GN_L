212c83f70dbba4ea0be989843b7fa3f1fa84dc27
Furfin <furf1n228@gmail.com>
2022-10-24 23:07:15 +0300

    init

---
+++ lab3/Location.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( o == this ) {
+            return true;
+        } else if ( o instanceof Location) {
+            Location l = (Location) o;
+            return xCoord==l.xCoord && yCoord==l.yCoord;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
--- prac/Practice2.java
+++ prac/Practice2.java
@@ -35,7 +35,6 @@ public class Practice2 {
         for(int i = 0; i< nums.length;i++) { 
             sum+= nums[i];
         }
-
         return sum%nums.length == 0;
     }
 
@@ -66,7 +65,6 @@ public class Practice2 {
                 point = true;
             }
         }
-
         return dP;
     }
 
